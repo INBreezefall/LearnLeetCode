3f2ec137520fd1fc06c7ee23c4b4609e6e3894ac
inbreeze <majj8@chinatelecom.cn>
2025-08-04 15:23:18 +0800

    feat: Three medium-level questions have been added (Two-pointer solution)

--- src/main/scala/top_100_liked/three_sum.scala
+++ src/main/scala/top_100_liked/three_sum.scala
@@ -29,7 +29,7 @@ import scala.collection.mutable
  * }}}
  */
 object three_sum {
-  private val example: Array[Int] = Array(-1, 0, 1, 0)
+  private val example: Array[Int] = Array(-2, -3, 0, 0, -2)
   private val example_output: Seq[Seq[Int]] = Seq(Seq(-1, -1, 2), Seq(-1, 0, 1))
 
   /**
@@ -40,7 +40,7 @@ object three_sum {
    * <li>2.三数同 || 两数同 需要独立判断 + 题目没仔细看
    * </ul>
    * <p>整个算法 整体上 虽然思路简单, 但整体不佳
-   * 
+   *
    * @param nums 输入数组
    * @return
    */
@@ -94,15 +94,18 @@ object three_sum {
     resultSet.toList
   }
 
-
   /**
-   * 读错题实现: 不重用元素 输出 和为零的三元组 [单端队列]
+   * 官解: 双指针
+   * 思路: 1.先排序, 得到升序数组, [-1, 0, 1, 2] -- 用于跳过重复元素
+   * 2.查找目标 nums[i] + nums[j] == -nums[k]
+   * 归纳: 全为正数/负数 不可能三数为 0, 一定存在 <= 0 的元素 [最特殊为 (0,0,0,0,0)]
+   * 其余情况 则为 [-负数 = 负/正数 + 负/正数]
    *
    * @param nums 输入数组
    * @return
    */
-  def threeSumWithNonRepetitiveElements(nums: Array[Int]): List[List[Int]] = {
-    // 0.边界判断
+  private def improved_threeSum(nums: Array[Int]): List[List[Int]] = {
+    // 0.1 边界判断
     if (nums.length < 3) {
       return List.empty
     } else if (nums.length == 3) {
@@ -112,77 +115,75 @@ object three_sum {
         return List.empty
       }
     }
+    // 0.2 构建排序数组
+    val sorted: Array[Int] = nums.sorted
+    //    println(s"[Print] sorted: ${sorted.mkString("Array(", ", ", ")")}")
 
-    // 1.1 单端队列准备
-    val queue: mutable.Queue[Int] = mutable.Queue(nums *)
-    // 1.2 K-Cnt Map 准备
-    val keyWithCount: mutable.Map[Int, Int] = nums
-      .groupBy(i => i)
-      .map((k, arr) => (k, arr.length))
-      .to(mutable.Map)
-    // 1.3 待跳过的 队中第三元素 Map
-    val thirdElemMap: mutable.Map[Int, Int] = mutable.HashMap[Int, Int]()
-
-    // 1.4 cnt - 1 函数定义
-    def decCountFromSpecialMap(key: Int, map: mutable.Map[Int, Int]): Unit = {
-      println(s"decCount $key")
-      map.updateWith(key) {
-        // Count - 1
-        case Some(c) if c >= 2 => println(s"$key.count = $c - 1"); Some(c - 1)
-        // 删除映射
-        case Some(c) if c == 1 => println(s"$key == 0"); None
-        // 不应该走到的地方
-        case _: Option[Int] => throw new Exception("Mistakes that should not occur")
-      }
-    }
-
-    // 1.5 cnt + 1 函数定义
-    def incCountFromSpecialMap(key: Int, map: mutable.Map[Int, Int]): Unit = {
-      println(s"incCount $key")
-      map.updateWith(key) {
-        // None + 1
-        case None => println(s"$key None + 1 = 1"); Some(1)
-        // c + 1
-        case Some(c) => println(s"$key.count = $c + 1"); Some(c + 1)
-        // 不应该走到的地方
-        case _: Option[Int] => throw new Exception("Mistakes that should not occur")
-      }
-    }
-
-    // 1.6 获取队首元素 碰撞 thirdElemMap 消除
-    def dequeueWithCrash(): Int = {
-      var v: Int = queue.dequeue()
-      if (thirdElemMap.contains(v)) {
-        println(s"dequeue crash $v")
-        decCountFromSpecialMap(v, thirdElemMap)
-        v = dequeueWithCrash()
+    // 0.3 快速反应
+    if (sorted.head == sorted.last && sorted.last == 0) {
+      // 0.3.1 纯 0 快反
+      return List(List(0, 0, 0))
+    } else if (sorted.head > 0 || sorted.last < 0) {
+      // 0.3.2 纯 正/负 快反
+      return List.empty
+    } else if (sorted.head == 0 || sorted.last == 0) {
+      // 0.3.3 含 0 纯 正 判断 0 数量
+      if (sorted.count(_ == 0) >= 3) {
+        return List.empty
       } else {
-        println(s"dequeue didn't crash $v")
-      }
-      v
-    }
-
-    // 2.循环 直到 队列元素 < 3
-    val result: mutable.ArrayBuffer[List[Int]] = mutable.ArrayBuffer[List[Int]]()
-    while (queue.size >= 3) {
-      val i: Int = dequeueWithCrash()
-      val j: Int = dequeueWithCrash()
-      val k: Int = -i - j
-      Seq(i, j).foreach(e => decCountFromSpecialMap(e, keyWithCount))
-      if (keyWithCount.contains(k)) {
-        println(s"hit the target: $i, $j, $k")
-        result.addOne(List(i, j, k))
-        decCountFromSpecialMap(k, keyWithCount)
-        incCountFromSpecialMap(k, thirdElemMap)
+        return List.empty
       }
     }
 
-    // 3.返回结果
-    result.toList
+    // 1.构建结果数组 & 循环
+    // ArrayBuilder 高效一次性生成不可变数组 | ArrayBuffer 更加通用
+    //    val res: mutable.ArrayBuilder[List[Int]] = mutable.ArrayBuilder.make[List[Int]]()
+    val res: mutable.ArrayBuffer[List[Int]] = mutable.ArrayBuffer[List[Int]]()
+    // 以最小元素作为 target, 逐步右移
+    var targetIndex = 0
+    import scala.util.control.Breaks._
+    while (targetIndex < nums.length && sorted(targetIndex) <= 0) {
+      breakable {
+        // 1.1 重复跳过
+        if (targetIndex > 0 && sorted(targetIndex) == sorted(targetIndex - 1)) {
+          targetIndex += 1
+          break()
+        }
+        //        println(s"[Print] target: ${-sorted(targetIndex)} targetIndex: $targetIndex")
+        // 1.2 左右双指针
+        var left: Int = targetIndex + 1
+        var right: Int = nums.length - 1
+        val target: Int = -sorted(targetIndex)
+
+        // 1.3 左右指针相向遍历 直到相遇停止 [是否有更好的提前停止]
+        while (left < right) {
+          // 1.3.1 判断是否三数和 = 0
+          val sum: Int = sorted(left) + sorted(right)
+          //          println(s"[Print] sum: $sum = ${sorted(left)} [$left] + ${sorted(right)} [$right]")
+          if (sum == target) {
+            //            println(s"[Print] hit: ${sorted(targetIndex)}, ${sorted(left)}, ${sorted(right)}")
+            res += List(sorted(targetIndex), sorted(left), sorted(right))
+            // 1.3.2 l & r 更新
+            left += 1
+            right -= 1
+            // 1.3.3 l & r 跳过重复
+            while (left < right && sorted(left) == sorted(left - 1)) left += 1
+            while (left < right && sorted(left) == sorted(right + 1)) right -= 1
+          } else if (sum < target) {
+            left += 1
+          } else {
+            right -= 1
+          }
+        }
+        targetIndex += 1
+      }
+    }
+    // res.result().toList
+    res.toList
   }
 
 
   def main(args: Array[String]): Unit = {
-    println(threeSum(example))
+    println(improved_threeSum(example))
   }
 }
